3369c81a70bb9d1a37924c1d183348e0de70a8a0
SushanthK07 <sushanthkummitha@gmail.com>
2024-02-23 00:22:51 +0530

    disable github api fetching projects for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           height="250px"
         />
       </HeroFlex>
-      <Projects />
+      {/* <Projects /> */}
       <Timeline />
     </Container>
   );
